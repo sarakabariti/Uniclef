a09dc3aab79f148b121494e37ae9402d4076bf1d
SaraKabariti <sairkab@hotmail.com>
2023-10-22 12:52:47 +0200

    Postgres Setup & Migrate

--- uniclef/settings.py
+++ uniclef/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'uniclef.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uniclefdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
