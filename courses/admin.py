8d7e3e4bd23e48fd2afc1c3240d82988699d4ba1
SaraKabariti <sairkab@hotmail.com>
2023-10-24 00:50:46 +0200

    Customize Admin Display Data

--- courses/admin.py
+++ courses/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Course
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'price', 'release_date', 'instructor')
+  list_display_links = ('id', 'title')
+  list_filter = ('instructor',)
+  search_fields = ('title', 'description', 'difficulty', 'duration', 'price')
+  list_per_page = 25
+
+admin.site.register(Course, CourseAdmin)
--- instructors/admin.py
+++ instructors/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Instructor
 
-admin.site.register(Instructor)
+
+class InstructorAdmin(admin.ModelAdmin):
+  list_display = ('id', 'first_name', 'last_name', 'email')
+  list_display_links = ('id', 'first_name', 'last_name')
+  search_fields = ('first_name', 'last_name')
+  list_per_page = 25
+
+admin.site.register(Instructor, InstructorAdmin)
