f4f5f9fa3b582171b0c751c3c8da6e4756018ff9
SaraKabariti <sairkab@hotmail.com>
2023-10-23 21:03:38 +0200

    Course and Instructor models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
+from instructors.models import Instructor
 
-# Create your models here.
+class Course(models.Model):
+    DIFFICULTY_CHOICES = (
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    difficulty = models.CharField(max_length=20, choices=DIFFICULTY_CHOICES)
+    duration = models.PositiveIntegerField()  # Duration in weeks
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')  
+    instructor = models.ForeignKey(Instructor, on_delete=models.DO_NOTHING)  
+    release_date = models.DateTimeField(default=datetime.now, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
--- instructors/models.py
+++ instructors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from courses.models import Course
 
-# Create your models here.
+class Instructor(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    email = models.EmailField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    courses = models.ForeignKey(Course, on_delete=models.DO_NOTHING) 
+    is_mvp = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
