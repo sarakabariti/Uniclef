72d86a8d8d61df893f2dc85dde0b7051ac277e4b
SaraKabariti <sairkab@hotmail.com>
2023-10-27 12:45:04 +0200

    Superuser

--- uniclef/settings.py
+++ uniclef/settings.py
@@ -94,7 +94,9 @@ DATABASES = {
 AUTH_USER_MODEL = 'users.User'
 
 
-
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
